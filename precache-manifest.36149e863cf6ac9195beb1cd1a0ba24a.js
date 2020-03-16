e5dac9b2390497b0ab66544a6590fc70bff7913d
unknown <belalabdulnasser@gmail.com>
2020-03-16 22:56:51 +0200

    Updates

---
+++ precache-manifest.36149e863cf6ac9195beb1cd1a0ba24a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef40e816559dd79ecf716e9d81859a5",
+    "url": "/monstersApp/index.html"
+  },
+  {
+    "revision": "8f346dfe891b11baac24",
+    "url": "/monstersApp/static/css/main.a581d577.chunk.css"
+  },
+  {
+    "revision": "df7d5fe324e5709d82cf",
+    "url": "/monstersApp/static/js/2.c726483c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monstersApp/static/js/2.c726483c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f346dfe891b11baac24",
+    "url": "/monstersApp/static/js/main.6aa09fd6.chunk.js"
+  },
+  {
+    "revision": "741b15d0dce01866b4cd",
+    "url": "/monstersApp/static/js/runtime-main.dbba5077.js"
+  }
+]);
